0d765b61091a9a7f36f5dbccff2efa292b115841
CalKK <158546364+CalKK@users.noreply.github.com>
2025-07-09 23:28:14 +0300

    Add files via upload

---
+++ src/data/education.ts
@@ -0,0 +1,114 @@
+export const education = [
+  {
+    institution: "Strathmore University",
+    degree: "Bachelor of Science in Electronics Engineering",
+    duration: "2021 - Present"
+  },
+  {
+    institution: "Strathmore High School",
+    duration: "2017 - 2021"
+  }
+];
+
+export const certifications = [
+  {
+    name: "POWER THE COMMUNITY",
+    issuer: "Energy Mentors",
+    date: "June 2025",
+    link: "https://drive.google.com/file/d/1-UP6TDLO0SN7R3N3MPp4bdWWvWA5xC47/view?pli=1"
+  },
+  {
+    name: "WED 2025",
+    issuer: "UNESCO World Engineering Day for Sustainable Development",
+    date: "March 2025",
+    link: "https://credsverse.com/credentials/0284c381-2d70-40f0-a633-0de7aa816376"
+  },
+  {
+    name: "HEP-SSA INNOVATION CHALLENGE",
+    issuer: "Royal Academy of Engineering",
+    date: "December 2023",
+    link: "https://drive.google.com/file/d/16x1oYNUKu-5cjhUAudAluz5uAftQBNmY/view?usp=drive_link"
+  },
+  {
+    name: "PROJECT DEVELOPMENT",
+    issuer: "IEEE YESIST12",
+    date: "July 2023",
+    link: "https://drive.google.com/file/d/1kCDPnlY8jbC4EnVAh2k55mgD5bXChzXX/view"
+  },
+  {
+    name: "DEVELOPMENT iTEAM NAIROBI PROGRAM",
+    issuer: "Centre for Global Equality",
+    date: "April 2023",
+    link: "https://drive.google.com/file/d/1_d4SR3n3mIWaavqimF5Es0muVTgdD1oa/view"
+  }
+];
+
+export const awards = [
+  {
+    name: "DISTINGUISHED DESIGN AWARD",
+    issuer: "Energy Mentors",
+    date: "June 2025",
+    description: "Scooped the accolade for Distinguished Design Award at the Power The Community Award Show on 31st of May 2025 for the solution of electricity as a service (EaaS) integrating grid-tied (solar) & off-grid solutions (battery swapping)."
+  },
+  {
+    name: "LEADERSHIP AWARD",
+    issuer: "Strathmore University",
+    date: "July 2024",
+    description: "Awarded for having served a 3 year tenure as Class Representative of my class."
+  },
+  {
+    name: "VICTORS AT IEEE KENYA STUDENT AND YOUNG PROFESSIONALS SMART LIVING CONFERENCE",
+    issuer: "IEEE KSYP",
+    date: "December 2024",
+    description: "Awarded for emerging victors at the Kenya Student and Young Professionals smart living conference."
+  },
+  {
+    name: "VICTORS AT ENACTUS KENYA X SCHNEIDER ELECTRIC BATTERY INNOVATION CHALLENGE NATIONALS",
+    issuer: "ENACTUS KENYA X SCHNEIDER ELECTRIC",
+    date: "September 2024",
+    description: "Awarded for emerging victors at the inaugural Enactus Kenya & Schneider Electric Battery Innovation Challenge nationals on 13th September of 2024."
+  },
+  {
+    name: "VICTORS AT ENACTUS KENYA X SCHNEIDER ELECTRIC BATTERY INNOVATION CHALLENGE REGIONALS",
+    issuer: "ENACTUS KENYA X SCHNEIDER ELECTRIC",
+    date: "July 2024",
+    description: "Awarded for emerging victors at the inaugural Enactus Kenya & Schneider Electric Battery Innovation Challenge regionals on 26th July of 2024."
+  },
+  {
+    name: "1ST RUNNERS UP",
+    issuer: "Royal Academy of Engineering",
+    date: "July 2024",
+    description: "Awarded for emerging 1st runners up in the Higher Education Partnerships in Sub-Saharan Africa (HEP-SSA) Innovation Challenge 2023 driven by the Royal Academy of Engineering on 23rd November 2023."
+  }
+];
+
+export const experience = [
+  {
+    role: "CO-FOUNDER & CPO",
+    company: "AFTERLIFE INNOVATIONS",
+    duration: "May 2024 - Present",
+    description: "● Drive innovation in sustainable product offerings that support a circular economy. ● Define and manage the product roadmap for recycling technologies.",
+    technologies: ["Product Development", "3D Modelling", "Creativity"]
+  },
+  {
+    role: "EX-OFFICIO ESA BOARD ADVISOR",
+    company: "SCESA (Strathmore Computing & Engineering Students Association)",
+    duration: "June 2025 - Present",
+    description: "● Offer counsel, sound mind decision making.",
+    technologies: ["Communication", "Coordination", "Creativity"]
+  },
+  {
+    role: "SYSTEMS ENGINEERING ATTACHE",
+    company: "UTU Cars Africa",
+    duration: "April 2025 - June 2025",
+    description: "● Drive innovation in sustainable product offerings.",
+    technologies: ["Product Development", "3D Modelling", "Creativity"]
+  },
+  {
+    role: "CHAIR & CTO",
+    company: "Institute of Engineering & Technology Strathmore Chapter",
+    duration: "January 2024 - December 2024",
+    description: "● Key signatory/officiator to all official documents of the society. Key decision maker. ● Chairs Meetings.",
+    technologies: ["Leadership", "Team Coordination", "Creativity"]
+  }
+];
---
+++ src/data/skills.ts
@@ -0,0 +1,19 @@
+import { FaTools, FaCube, FaMicrochip } from 'react-icons/fa';
+
+export const skills = [
+  {
+    name: "E-mobility",
+    icon: FaTools,
+    technologies: ["CBA IV Software", "TinkerCAD", "Fusion 360"]
+  },
+  {
+    name: "3D Modelling",
+    icon: FaCube,
+    technologies: ["AUTOCAD", "TinkerCAD"]
+  },
+  {
+    name: "PCB Design",
+    icon: FaMicrochip,
+    technologies: ["EasyEDA", "kiCAD"]
+  }
+];
